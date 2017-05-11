027ded6e2245f4d42d00dcfd3426b310fb78fee5
primatebuddy <primatebuddy@gmail.com>
2017-05-11 08:30:54 -0400

    added more to candp

--- candp.sh
+++ candp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+################################################################################
+## NAME: candp.sh
+## PURPOSE: Simplify git adds. This will add, commit, and push ALL changes to
+##          files in a given git repo.
+## DATA DICTIONARY:
+##    $1: the commit comment
+## NOTES: Remember to add the ssh key to the github repo.
+################################################################################
 git add -A
 git commit -m "$1"
 git push -uv origin master
