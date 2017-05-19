048519496b0830dcb500321551b43f935c66115f
Primatebuddy <primatebuddy@gmail.com>
2017-05-19 02:10:38 -0400

    NA

--- reddit_challenges/2170510-314/314.c
+++ reddit_challenges/2170510-314/314.c
@@ -25,25 +25,44 @@ def lcs(S):
 int main(int argc, char const *argv[]) {
   char const *tmp = argv[1];
   char s[strlen(tmp)];
-  int f[(-1) * strlen(s)];
-  int i, j, k;
+  int f[strlen(s)];
+  int i, j, k, t;
   char sj;
 
   //Booth's algorithm
   strcpy(s, tmp);
   strcat(s, tmp);
+  for(t=0; t<strlen(s); t++)
+    f[t] = -1;
   k = 0;
 
-  for(j=1; j<=strlen(s); j++) {
+  for(j=1; j <= strlen(s); j++) {
     sj = s[j];
-
+    i = f[j-k-1];
+    while(!(i == -1) && !(sj == s[k+i+1])) {
+      if(sj < s[k+i+1]) {
+        k = j-i-1;
+      }
+      i = f[i];
+    }
+    if(sj != s[k+i+1]) {
+      if(sj < s[k]) {
+        k = j;
+      }
+      f[j-k] = -1;
+    }
+    else {
+      f[j-k] = i+1;
+    }
   }
 
+  //return k;
 
 
 
 
-  printf("%s\n", s);
+
+  printf("%d %s\n", k, s);
 
 
 
