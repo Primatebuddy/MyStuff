43a8e9a2b410915cee23de7d4f031c1f85c9189b
Primatebuddy <primatebuddy@gmail.com>
2017-05-20 10:06:03 -0400

    change print style and added comments

--- reddit_challenges/20170519-277/277.py
+++ reddit_challenges/20170519-277/277.py
@@ -1,18 +1,25 @@
+import sys
+
 def gcd(a, b):
 
+    # find out which is larger and set the range top var
 	if a-b < 0:
 		x = a
 	else:
 		x = b
 
+    # modular arithmetic again! I have an unreasonable attraction here...
 	for i in range(x, 2, -1):
 		if a%i == 0 and b%i == 0:
 			return i
-
 	return 1
 
-a = 1536
-b = 78360
-c = gcd(a,b)
+def main():
+    a = int(sys.argv[1])    # have to convert to an int, just like C
+    b = int(sys.argv[2])
+    c = gcd(a,b)            # store results in c
+    print("gcd={}  num={}  den={}".format(c, a/c, b/c))
 
-print("gcd=", c, "num=", a/c, "den=", b/c)
+# account for the main() implementation
+if __name__ == "__main__":
+    main()
