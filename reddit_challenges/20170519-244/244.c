9254356154a119fca20d1eeb227227d2401d67f0
Primatebuddy <primatebuddy@gmail.com>
2017-05-20 08:13:11 -0400

    na

--- reddit_challenges/20170519-244/244.c
+++ reddit_challenges/20170519-244/244.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int gcd(int, int);
 
 int main(int argc, char **argv) {
 
+  printf("%d\n", gcd((int)argv[1], (int)argv[2]));
+
   return 0;
+}
+
+int gcd(int a, int b) {
+  int i;
+
+  for(i=2; i<a; i++) {
+    if(a%i == 0 && b%i == 0)
+      return i;
+  }
+  return 1;  
 }
