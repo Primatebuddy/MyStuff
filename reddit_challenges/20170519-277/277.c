ed5bc45b7a507c893c20b7bd455313cb6cb3a06c
Primatebuddy <primatebuddy@gmail.com>
2017-05-20 09:18:18 -0400

    na

--- reddit_challenges/20170519-277/277.c
+++ reddit_challenges/20170519-277/277.c
@@ -4,9 +4,9 @@
 int gcd(int, int);
 
 int main(int argc, char **argv) {
-  int num = atoi(argv[1]);
+  int num = atoi(argv[1]);  //casting doesn't work here, so use atoi
   int den = atoi(argv[2]);
-  int res = gcd(num, den);
+  int res = gcd(num, den);  //store the results
 
   printf("%d %d\n", num/res, den/res);
 
@@ -16,11 +16,13 @@ int main(int argc, char **argv) {
 int gcd(int a, int b) {
   int i;
 
+  //check to see which of the params are smaller, and set i to that
   if((a-b) < 0)
     i=a;
   else
     i=b;
 
+  //the meat of gcd, modular arithmetic!
   for(i=a; i>1; i--) {
     if(a%i == 0 && b%i == 0)
       return i;
